9485efa265e0c7d6bf263aa64d01e605b1a1fadd
kozhubluk <nastya.awdeeva@yandex.ru>
2022-11-19 16:29:13 +0300

    Каталог

---
+++ catalog/scripts/card_buttons.js
@@ -0,0 +1,25 @@
+const btn = document.querySelectorAll('.add_to_cart');
+const heart = document.querySelectorAll('.add_to_fav');
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', function() {
+   if (this.innerHTML === "В корзине"){
+      this.innerHTML = "В корзину";
+      this.classList.remove("active");
+   }
+   else{
+      this.innerHTML = "В корзине";
+      this.classList.add("active");
+   }
+  })
+}
+for (let i = 0; i < heart.length; i++) {
+   heart[i].addEventListener('click', function() {
+   // addEventListener(type, listener) type - тип события listner - функция
+         if (this.classList.contains("liked")) { 
+           this.classList.remove("liked"); // добавляем класс элементу
+         } else {
+           this.classList.add("liked");
+         }
+       ;
+   })
+}
